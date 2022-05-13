134a8459b184fc27ad478b38bba69ccc8eda3f0c
jannatul114 <jfmithila114@gmail.com>
2022-05-14 01:50:25 +0600

    heroku

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 
-//middleware
+//middlewares 
 app.use(cors())
 app.use(express.json())
 
@@ -54,5 +54,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`doctors app listening on port ${port}`)
 })
